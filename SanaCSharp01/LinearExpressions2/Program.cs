384d0820af142535e6fa468c279eeed1591bfc03
Viacheslav Varnavskyi <ipz224_vvo@student.ztu.edu.ua>
2022-11-26 20:47:25 +0200

    Додано 2 завершені проекти

---
+++ SanaCSharp01/LinearExpressions2/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace LinearExpressions2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double m, n;
+            Console.Write("Enter value of 'm': ");
+            m = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter value of 'n': ");
+            n = Convert.ToDouble(Console.ReadLine());
+
+            double z1 = ((m - 1) * Math.Sqrt(m) - (n - 1) * Math.Sqrt(n)) / (Math.Sqrt(m * m * m * n) + n * m + m * m - m);
+            Console.WriteLine($"z1={z1}");
+            double z2 = (Math.Sqrt(m) - Math.Sqrt(n)) / m;
+            Console.WriteLine($"z2={z2}");
+
+            double x, a, b;
+            Console.Write("Enter value of 'x': ");
+            x = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter value of 'a': ");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter value of 'b': ");
+            b = Convert.ToDouble(Console.ReadLine());
+
+            double y = 2.4 * Math.Abs((x * x + b) / a) + (a - b) * Math.Pow(Math.Sin(a - b), 2) + (x - b) / 100;
+            Console.WriteLine($"y={y}");
+        }
+    }
+}
