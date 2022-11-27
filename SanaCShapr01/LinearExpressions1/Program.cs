fdc70e561bfff964b81e6514b232d30ce7294c0f
Sergiy Marchuk <sana22_msv@student.ztu.edu.ua>
2022-11-27 20:07:18 +0200

    Practical work 03

---
+++ SanaCShapr01/LinearExpressions1/Program.cs
@@ -0,0 +1,28 @@
+﻿double a, b, c, d;
+Console.WriteLine("Fractional number a:");
+a = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Fractional number b:");
+b = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Fractional number c:");
+c = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Fractional number d:");
+d = double.Parse(Console.ReadLine());
+
+double resultX = ((a + 2 * b - c + d) / (c * d)) + ((a + b) / (c - d)) - ((a * a) / (b * b));
+Console.WriteLine($"X: \n{resultX}");
+
+double resultY = ((5 * (a + b) * (c - d)) / (0.5 * c)) + (d * d) * (((a * a) - (b * b)) / (b - a));
+Console.WriteLine($"Y: \n{resultY}");
+
+double resultZ = (Math.Pow(resultX * resultX - 2 * resultX, 3) - 4 * (Math.Pow(resultX, 4) + 1) * (1 - b)) / (5 * a + 3 * b);
+Console.WriteLine($"Z: \n{resultZ}");
+
+double resultR = ((0.5 * a) + (3 / 4 * b) - (7 / 5)) / (3 * c + 1) + (1 / (a - c));
+Console.WriteLine($"R: \n{resultR}");
+
+
+
+
---
+++ SanaCShapr01/LinearExpressions2/Program.cs
@@ -0,0 +1,26 @@
+﻿double m, n;
+Console.WriteLine("Fractional number m:");
+m = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Fractional number n:");
+n = double.Parse(Console.ReadLine());
+
+double resultZ1 = ((m - 1) * Math.Sqrt(m) - (n - 1) * Math.Sqrt(n)) / (Math.Sqrt(m * m * m * n) + n * m + m * m - m);
+Console.WriteLine($"Z1: \n{resultZ1}");
+
+double resultZ2 = (Math.Sqrt(m) - Math.Sqrt(n)) / (m);
+Console.WriteLine($"Z2: \n{resultZ2}");
+
+
+double a, b, x;
+Console.WriteLine("number a:");
+a = double.Parse(Console.ReadLine());
+
+Console.WriteLine("number b:");
+b = double.Parse(Console.ReadLine());
+
+Console.WriteLine("number x:");
+x = double.Parse(Console.ReadLine());
+
+double resultY = (2.4 * Math.Abs((x * x + b) / a)) + ((a - b) * Math.Pow(Math.Sin(a - b), 2)) + ((Math.Pow(10, -2)) * (x - b));
+Console.WriteLine("Y = {0}", resultY);
