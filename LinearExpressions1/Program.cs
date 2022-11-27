a0af68bbd778fc640c42014f5eb98d519837fdae
Viktor Opanasyuk <ipz223-ovs@student.ztu.edu.ua>
2022-11-27 20:21:13 +0200

    Completed practical work

---
+++ LinearExpressions1/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Enter the four numbers - a, b, c ,d.");
+double a, b, c, d;
+Console.Write("Enter the \'a\': ");
+if (!double.TryParse(Console.ReadLine(), out a)) return;
+Console.Write("Enter the \'b\': ");
+if (!double.TryParse(Console.ReadLine(), out b)) return;
+Console.Write("Enter the \'c\': ");
+if (!double.TryParse(Console.ReadLine(), out c)) return;
+Console.Write("Enter the \'d\': ");
+if (!double.TryParse(Console.ReadLine(), out d)) return;
+Console.WriteLine($"You enter a={a}, b={b}, c={c}, d={d}.");
+if (c != 0 && d != 0 && c != d && b != 0 && b != a && a != c && c != 1 / 3)
+{
+    double x, y, z, r;
+    x = (a + 2 * b - c + d) / (c * d) + (a + b) / (c - d) - Math.Pow(a, 2) / Math.Pow(b, 2);
+    y = (5 * (a + b) * (c - d)) / (1 / 2 * c) + Math.Pow(d, 2) * (Math.Pow(a, 2) - Math.Pow(b, 2)) / (b - a);
+    z = ((Math.Pow(Math.Pow(x, 2) - 2 * x, 3) - 4 * (Math.Pow(x, 4) + 1)) * (1 - b)) / (5 * a + 3 * b);
+    r = (1 / 2 * a + 3 / 4 * b - 7 / 5) / (3 * c + 1) + 1 / (a - c);
+    Console.Write($"1. x = {x}\n" +
+        $"2. y = {y}\n" +
+        $"3. z = {z}\n" +
+        $"4. r = {r}\n");
+}
+else
+    Console.WriteLine("Incorect numbers");
---
+++ LinearExpressions2/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Enter the four numbers - m,n,x,b,a.");
+double m, n, x, a , b;
+Console.Write("Enter the \'m\': ");
+if (!double.TryParse(Console.ReadLine(), out m)) return;
+Console.Write("Enter the \'n\': ");
+if (!double.TryParse(Console.ReadLine(), out n)) return;
+Console.Write("Enter the \'a\': ");
+if (!double.TryParse(Console.ReadLine(), out a)) return;
+Console.Write("Enter the \'b\': ");
+if (!double.TryParse(Console.ReadLine(), out b)) return;
+Console.Write("Enter the \'x\': ");
+if (!double.TryParse(Console.ReadLine(), out x)) return;
+Console.WriteLine($"You enter a={m}, b={n}.");
+if (m!=0)
+{
+    double z1, z2, y;
+    z1 = ((m-1)*Math.Sqrt(m)-(n-1)*Math.Sqrt(n)) / (Math.Sqrt(Math.Pow(m, 3) * n) + n * m + Math.Pow(m, 2) - m);
+    z2 = (Math.Sqrt(m) - Math.Sqrt(n)) / m;
+    y = 2.4 * Math.Abs((Math.Pow(x, 2) + b) / a) + (a - b) * Math.Sin(Math.Pow(a - b, 2)) + Math.Pow(10, -2) * (x - b);
+    Console.Write($"1. z1 = {z1}\n" +
+        $"2. z2 = {z2}\n" +
+        $"3. y = {y}\n");
+}
+else
+    Console.WriteLine("Incorect numbers");
