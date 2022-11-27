29a348d44ff8721bc7478745c339d417d2e29f5d
Verkhohliad Denys <sana22_vdv@student.ztu.edu.ua>
2022-11-28 00:49:31 +0200

    LinearExpressions2 done

--- LinearExpressions1/Program.cs
+++ LinearExpressions1/Program.cs
@@ -22,4 +22,3 @@ z = ((Math.Pow((x * x) - 2 * x, 3) - 4 * (Math.Pow(x, 4) + 1)) * (1 - b)) / (5 *
 r = (((1 / 2) * a + (3 / 4) * b - 7 / 5) / 3 * c + 1) + 1 / (a - c);
 
 Console.WriteLine($"Результати обчислень виразів:\n x = {x}\n y = {y}\n z = {z}\n r = {r}");
-Console.ReadKey();
