452e2c1a707e0ff15626b297a4980afa4266e975
Ivan Savchuk <ipzm222_sio@student.ztu.edu.ua>
2022-11-28 13:26:01 +0200

    Linear expressions 2 solution added

--- SanaCSharp01/LinearExpressions1/Program.cs
+++ SanaCSharp01/LinearExpressions1/Program.cs
@@ -1,6 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿System.Globalization.CultureInfo customCulture = (System.Globalization.CultureInfo)Thread.CurrentThread.CurrentCulture.Clone();
+customCulture.NumberFormat.NumberDecimalSeparator = ".";
+Thread.CurrentThread.CurrentCulture = customCulture;
 
-Console.WriteLine("Please, enter 4 numbers for calculation.");
+
+Console.WriteLine("Please, enter 4 numbers for calculations.");
 
 Console.Write("Enter first number (a): ");
 double a = Convert.ToDouble(Console.ReadLine());
@@ -16,14 +19,16 @@ double d = Convert.ToDouble(Console.ReadLine());
 
 
 double x = ((a + (2 * b) - c + d) / (c * d)) + ((a + b) / (c - d)) - (Math.Pow(a, 2) / Math.Pow(b, 2));
-Console.WriteLine($"\nx = {x}");
+Console.WriteLine($"\nx = {Math.Round(x, 4)}");
 
 double y = (5 * ((a + b) * (c - d))) / (0.5 * c) + (Math.Pow(d, 2) * (((Math.Pow(a, 2)) - Math.Pow(b, 2)) / (b - a)));
-Console.WriteLine($"\ny = {y}");
+Console.WriteLine($"\ny = {Math.Round(y, 4)}");
 
-double z = ((Math.Pow((Math.Pow(x, 2) - 2 * x), 3) - 4 * (Math.Pow(x, 4) + 1)) * (1 - b)) / ((5 * a) + (3 * b));
+double z = ((Math.Pow(Math.Pow(x, 2) - (2 * x), 3) - 4 * (Math.Pow(x, 4) + 1)) * (1 - b)) / ((5 * a) + (3 * b));
 
-Console.WriteLine($"\nz = {z}");
+Console.WriteLine($"\nz = {Math.Round(z, 4)}");
 
-double r = (((0.5 * a) + ((3.0 / 4.0) * b) - (7.0 / 5.0)) / ((3.0 * c) + 1.0)) + (1.0 / (a - c));
-Console.WriteLine($"\nr = {r}");
+double r = ((0.5 * a + ((3.0 / 4.0) * b) - (7.0 / 5.0)) / ((3.0 * c) + 1.0)) + (1.0 / (a - c));
+Console.WriteLine($"\nr = {Math.Round(r, 4)}");
+
+Console.ReadKey();
--- SanaCSharp01/LinearExpressions2/Program.cs
+++ SanaCSharp01/LinearExpressions2/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿System.Globalization.CultureInfo customCulture = (System.Globalization.CultureInfo)Thread.CurrentThread.CurrentCulture.Clone();
+customCulture.NumberFormat.NumberDecimalSeparator = ".";
+Thread.CurrentThread.CurrentCulture = customCulture;
+
+
+Console.WriteLine("Please, enter 5 numbers for calculations.");
+
+Console.Write("Enter first number (a): ");
+double a = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Enter second number (b): ");
+double b = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Enter third number (x): ");
+double x = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Enter last number (m): ");
+double m = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Enter last number (n): ");
+double n = Convert.ToDouble(Console.ReadLine());
+
+
+double z1 = ((m -1) * Math.Sqrt(m) - (n - 1) * Math.Sqrt(n)) / (Math.Sqrt(Math.Pow(m, 3) * n) + (n * m) + Math.Pow(m, 2) - m);
+Console.WriteLine($"\nz1 = {Math.Round(z1, 4)}");
+
+double z2 = (Math.Sqrt(m) - Math.Sqrt(n)) / m;
+Console.WriteLine($"\nz2 = {Math.Round(z2, 4)}");
+
+double y = 2.4 * Math.Abs((Math.Pow(x, 2) + b) / a) + (a - b) * Math.Pow(Math.Sin(a - b), 2) + Math.Pow(10, -2) * (x - b);
+Console.WriteLine($"\ny = {Math.Round(y, 4)}");
+
+Console.ReadKey();
