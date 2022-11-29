a6df9a0926351ebccbbd1eb35d76626006d9a63a
VitaBrrevis <vitalina.doronina@gmail.com>
2022-11-29 18:57:19 +0200

    first commit of this laba

---
+++ LinearExpressions1/SanaCShapr01.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program {
+  public static void Main (string[] args) {
+   double a, b, c, d, x, y, z, r;
+
+Console.WriteLine("Enter the value of a");
+Console.Write("-> ");
+a = double.Parse(Console.ReadLine());
+Console.WriteLine("Enter the value of b");
+Console.Write("-> ");
+b = double.Parse(Console.ReadLine());
+Console.WriteLine("Enter the value of c");
+Console.Write("-> ");
+c = double.Parse(Console.ReadLine());
+Console.WriteLine("Enter the value of d");
+Console.Write("-> ");
+d = double.Parse(Console.ReadLine());
+
+x = (a+2*b-c+d)/(c*d)+(a+b)/(c-d)-Math.Pow(a, 2)/Math.Pow(b, 2);
+y = (5 * (a + b)*(c - d)) / (0.5 * c) + Math.Pow(d, 2) * (Math.Pow(a, 2) - Math.Pow(b, 2)) / (b - a);
+z = ((Math.Pow((Math.Pow(x, 2) - 2 * x), 3) - 4 * (Math.Pow(x, 4) + 1)) * (1 - b))/(5*a+3*b);
+r = ((a*1)/2 + (b* 3)/4  - 7/5)/(3 * c + 1) + (1 / (a - c));
+
+Console.WriteLine("the value of x -> " + x );
+Console.WriteLine("the value of y -> " + y );
+Console.WriteLine("the value of z -> " + z );
+Console.WriteLine("the value of r -> " + r );
+
+
+  }
+}
---
+++ LinearExpressions2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program {
+  public static void Main (string[] args) {
+    double a, b, x, m, n, y, z1, z2;
+
+Console.WriteLine("Enter the value of m");
+Console.Write("-> ");
+m = double.Parse(Console.ReadLine());
+Console.WriteLine("Enter the value of n");
+Console.Write("-> ");
+n = double.Parse(Console.ReadLine());
+
+z1 = ((m - 1) * Math.Sqrt(m) - (n - 1) * Math.Sqrt(n)) / (Math.Sqrt(Math.Pow(m, 3) * n) + n * m + Math.Pow(m, 2) - m);
+z2 = (Math.Sqrt(m) - Math.Sqrt(n)) / m;
+
+Console.WriteLine("the value of z1" + z1);
+Console.WriteLine("the value of z2"+z2);
+
+
+Console.WriteLine("Enter the value of a");
+Console.Write("-> ");
+a = double.Parse(Console.ReadLine());
+Console.WriteLine("Enter the value of b: ");
+Console.Write("-> ");
+b = double.Parse(Console.ReadLine());
+Console.WriteLine("Enter the value of x: ");
+Console.Write("-> ");
+x = double.Parse(Console.ReadLine());
+
+y = 2.4 * Math.Abs((Math.Pow(x, 2) + b) / a) + (a - b) * Math.Pow(Math.Sin(a - b), 2) + Math.Pow(10, -2) * (x - b);
+
+Console.WriteLine("the value of y: " + y);
+
+  }
+}
