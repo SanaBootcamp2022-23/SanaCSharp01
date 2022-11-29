9eca915ba43b96236059fae5cff485275b0464d5
Denis Yakovenko <kn202_yadv@student.ztu.edu.ua>
2022-11-29 21:02:15 +0200

    Додано файл LinearExpressions2, та реалiзовано введення значень (a,b,x,m,n)

---
+++ SanaCShapr01/LinearExpressions2/Program.cs
@@ -0,0 +1,17 @@
+﻿double a, b, x, m, n, z1, z2, y;
+
+Console.Write("Enter value a: ");
+a = double.Parse(Console.ReadLine());
+
+Console.Write("Enter value b: ");
+b = double.Parse(Console.ReadLine());
+
+Console.Write("Enter value x: ");
+x = double.Parse(Console.ReadLine());
+
+Console.Write("Enter value m: ");
+m = double.Parse(Console.ReadLine());
+
+Console.Write("Enter value n: ");
+n = double.Parse(Console.ReadLine());
+
