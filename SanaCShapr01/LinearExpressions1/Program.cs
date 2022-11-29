b4f68746ca0f19d64d224a62ec2703325b1df355
Denis Yakovenko <kn202_yadv@student.ztu.edu.ua>
2022-11-29 20:00:45 +0200

    Додано введення значень (a, b, c, d) для проекту LinearExpressions1

---
+++ SanaCShapr01/LinearExpressions1/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+double a, b, c, d;
+
+Console.Write("Enter value a: ");
+a = double.Parse(Console.ReadLine());
+
+Console.Write("Enter value b: ");
+b = double.Parse(Console.ReadLine());
+
+Console.Write("Enter value c: ");
+c = double.Parse(Console.ReadLine());
+
+Console.Write("Enter value d: ");
+d = double.Parse(Console.ReadLine());
+
