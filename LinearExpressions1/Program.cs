f89910a7d141c72f39337fe429b4cc9fd898d8b1
Oleksandra Kryvonos <ipzm222_kop@student.ztu.edu.ua>
2022-11-30 01:18:10 +0200

    Create Sana task 3

---
+++ LinearExpressions1/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Runtime.CompilerServices;
+
+double a, b, c, d, x, y, z, r;
+
+Console.WriteLine("Hello! Today we`ll solve 4 question question with fractional variables " +
+                    "\"a\", \"b\", \"c\" and \"d\".");
+Console.WriteLine("Please, use separator \".\" when you'll enter numbers.");
+Console.WriteLine("Lets start!");
+Console.WriteLine("");
+
+Console.WriteLine("Now, you should input the first fractional variable \"a\":");
+a = double.Parse(Console.ReadLine());
+Console.WriteLine("Graet! Input the second fractional variable \"b\":");
+b = double.Parse(Console.ReadLine());
+Console.WriteLine("Very well! Input the third fractional variable \"c\":");
+c = double.Parse(Console.ReadLine());
+Console.WriteLine("And finally, input the fourth fractional variable \"d\": ");
+d = double.Parse(Console.ReadLine());
+Console.WriteLine("");
+
+x = ((a + 2 * b - c + d) / (c * d) + (a + b) / (c - d) - (Math.Pow(a, 2) / Math.Pow(b, 2)));
+Console.WriteLine($"The result of (a+2b-c+d)/(cd)+(a+b)/(c-d)-(a^2)/(b^2) is: {x}");
+
+y = ((5 * (a + b) * (c - d)) / 0.5 * c + (Math.Pow(d, 2) * ((Math.Pow(a, 2) - Math.Pow(b, 2)) / (b - a))));
+Console.WriteLine($"The result of (5(a+b)(c-d))/(0.5*c)+(d^2*((a^2-b^2)/(b-a)) is: {y}");
+
+z = ((Math.Pow((Math.Pow(x, 2) - 2 * x), 3) - 4 * (Math.Pow(x, 4) + 1)) * (1 - b)) / (5 * a + 3 * b); 
+Console.WriteLine($"The result of ((x^2-2x)^3-4(x^4+1))(1-b)/(5a+3b) is: {z}");
+
+r = (0.5 * a + 3 / 4 * b - 7 / 5) / (3 * c + 1) + (1 / (a - c));
+Console.WriteLine($"The result of (1/2a+3/4b-7/5)/(3c+1)+1/(a-c) is: {r}");
---
+++ LinearExpressions2/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Runtime.CompilerServices;
+
+double z1, z2, y, m, n, x, b, a;
+
+Console.WriteLine("Hello! Let's solve 3 question with fractional variables \"m\", \"n\", \"x\", \"b\" and \"a\".");
+Console.WriteLine("Please, use separator \".\" when you'll enter numbers.");
+Console.WriteLine("Lets start!");
+Console.WriteLine("");
+
+Console.WriteLine("Now, you should input the fractional variable \"m\":");
+m = double.Parse(Console.ReadLine());
+Console.WriteLine("Graet! Input the fractional variable \"n\":");
+n = double.Parse(Console.ReadLine());
+Console.WriteLine("");
+
+z1 = (m - 1) * Math.Sqrt(m) - (n - 1) * Math.Sqrt(n) / (Math.Sqrt (Math.Pow(m,3) * n) +
+    n * m + Math.Pow(m,2) - m);
+Console.WriteLine($"Result of ((m-1)√m-(n-1)√n)/(√m^3n+nm+m^2-m) is: {z1}");
+z2 = (Math.Sqrt(m) - Math.Sqrt(n)) / m;
+Console.WriteLine($"Result of (√m-√n)/m is: {z2}");
+Console.WriteLine("");
+
+Console.WriteLine("And lets solve the last question!");
+Console.WriteLine("");
+
+Console.WriteLine("Now, you should input the fractional variable \"x\":");
+x = double.Parse(Console.ReadLine());
+Console.WriteLine("Graet! Input the fractional variable \"b\":");
+b = double.Parse(Console.ReadLine());
+Console.WriteLine("Graet! Input the fractional variable \"a\":");
+a = double.Parse(Console.ReadLine());
+Console.WriteLine("");
+
+y = 2.4 * Math.Abs( (Math.Pow(x,2) + b) / a) + (a - b) * Math.Pow(Math.Sin(a - b), 2)
+     + Math.Pow(10, -2) * (x - b);
+Console.WriteLine($"Result of 2.4|(x^2+b)/a|+(a-b)+10^-2(x-b) is: {y}");
