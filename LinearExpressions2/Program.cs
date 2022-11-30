bb9476990170858dd2efab92777f1ea6d1f6652a
filippdemchenko <filipp5632@gmail.com>
2022-11-30 15:55:03 +0200

    Add LinearExpressions2 console project

---
+++ LinearExpressions2/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace LinearExpressions2
+{
+    public class Program
+    {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello, world!");
+        }
+    }
+}
