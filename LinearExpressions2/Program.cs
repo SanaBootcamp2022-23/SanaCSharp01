21d56e01d0cfb6e617d0c788208d16753e0ca6d5
Roman Hrushnyk <sana22_gra@student.ztu.edu.ua>
2022-11-30 20:17:07 +0200

    Linear Expressions 2

---
+++ LinearExpressions2/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Введіть число (m)");
+double m = double.Parse(Console.ReadLine());
+
+
+Console.WriteLine("Введіть число (n)");
+double n = double.Parse(Console.ReadLine());
+
+
+Console.WriteLine("Введіть число (x)");
+double x = double.Parse(Console.ReadLine());
+
+
+Console.WriteLine("Введіть число (a)");
+double a = double.Parse(Console.ReadLine());
+
+
+Console.WriteLine("Введіть число (b)");
+double b = double.Parse(Console.ReadLine());
+
+
+double z1 = ((m - 1.0) * Math.Sqrt(m) - (n - 1.0) * Math.Sqrt(n)) / (Math.Sqrt(Math.Pow(m, 3) * n) + n * m + (m * m) - m);
+
+double z2 = (Math.Sqrt(m) - Math.Sqrt(n)) / m;
+
+double y = 2.4 * (Math.Abs((x * x + b) / a)) + (a - b) * Math.Pow(Math.Sin(a - b), 2) + Math.Pow(10, -2) * (x - b);
