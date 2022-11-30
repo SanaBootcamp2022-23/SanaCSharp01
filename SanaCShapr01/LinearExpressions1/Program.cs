8d312a0b43f5d0fe3b94997805ffd81b45706980
Walenty Sawicki <sana22_vsb@student.ztu.edu.ua>
2022-11-30 19:42:12 +0100

    Initialized variables

--- SanaCShapr01/LinearExpressions1/Program.cs
+++ SanaCShapr01/LinearExpressions1/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Simple program to calculate values. " +
+    "Powered by WAYLIIKE!");
+Console.WriteLine("Enter 4 values below to calculate x , y, z, r.");
+Console.WriteLine("Enter a: ");
+double a = double.Parse(Console.ReadLine());
+Console.WriteLine("Enter b: ");
+double b = double.Parse(Console.ReadLine());
+Console.WriteLine("Enter c: ");
+double c = double.Parse(Console.ReadLine());
+Console.WriteLine("Enter d: ");
+double d = double.Parse(Console.ReadLine());
+
+
