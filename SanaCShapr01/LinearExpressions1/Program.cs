3e0a62bfc7d67ab5ca5b1162d76d6ed291c79573
tabbbyzzxc <ipz221_mms@student.ztu.edu.ua>
2022-12-01 00:29:55 +0100

    Added LinearExpressions1 and LinearExpressions2

---
+++ SanaCShapr01/LinearExpressions1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter values: ");
+            Console.Write("a: ");
+            double a = int.Parse(Console.ReadLine());
+            Console.Write("b: ");
+            double b = int.Parse(Console.ReadLine());
+            Console.Write("c: ");
+            double c = int.Parse(Console.ReadLine());
+            Console.Write("d: ");
+            double d = int.Parse(Console.ReadLine());
+
+            double x = (a + 2 * b - c + d) / (c * d) + (a + b) / (c - d) - (a * a) / (b * b);
+            Console.WriteLine($"x: {x}");
+            double y = (5 * (a + b) * (c - d)) / (0.5 * c) + (d * d) * ((a * a) - (b * b)) / (b - a);
+            Console.WriteLine($"y: {y}");
+            double z = ((Math.Pow(x * x - 2 * x, 3) - 4 * (Math.Pow(x, 4) + 1)) * (1 - b)) / (5 * a + 3 * b);
+            Console.WriteLine($"z: {z}");
+            double r = (0.5 * a + 0.75 * b - 1.4) / (3 * c + 1) + 1 / (a - c);
+            Console.WriteLine($"r: {r}");
+        }
+    }
+}
---
+++ SanaCShapr01/LinearExpressions2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Xml;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter values: ");
+            Console.Write("m: ");
+            double m = int.Parse(Console.ReadLine());
+            Console.Write("n: ");
+            double n = int.Parse(Console.ReadLine());
+            Console.Write("x: ");
+            double x = int.Parse(Console.ReadLine());
+            Console.Write("a: ");
+            double a = int.Parse(Console.ReadLine());
+            Console.Write("b: ");
+            double b = int.Parse(Console.ReadLine());
+
+
+            double z1 = ((m - 1) * Math.Sqrt(m) - (n - 1) * Math.Sqrt(n)) / (Math.Sqrt(Math.Pow(m, 3) * n) + n * m + m * m - m);
+            Console.WriteLine($"z1: {z1}");
+
+            double z2 = (Math.Sqrt(m) - Math.Sqrt(n)) / m;
+            Console.WriteLine($"z2: {z2}");
+
+            double y = 2.4 * Math.Abs((x * x + b) / a) + (a - b) * Math.Pow(Math.Sin(a - b), 2) + 0.01 * (x - b);
+            Console.WriteLine($"y: {y}");
+
+        }
+    }
+}
