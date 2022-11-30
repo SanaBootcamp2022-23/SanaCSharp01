909c70b2d100d476f125524ce0fd0398123d95d4
Verkhohliad Denys <117022298+Werhogliad@users.noreply.github.com>
2022-12-01 00:13:59 +0200

    Bug fixed in LinearExpressions1

--- LinearExpressions1/Program.cs
+++ LinearExpressions1/Program.cs
@@ -17,8 +17,8 @@ Console.Write("Ще c - ");       c = double.Parse(Console.ReadLine());
 Console.Write("Накінець d - "); d = double.Parse(Console.ReadLine());
 
 x = ((a + 2 * b - c + d) / (c * d)) + ((a + b) / (c - d)) - (a * a) / (b * b);
-y = ((5 * (a + b) * (c - d)) / (1/2 * c)) + (d * d) * ((a * a - b * b) / (b - a));
+y = ((5 * (a + b) * (c - d)) / (1.0/2.0 * c)) + (d * d) * ((a * a - b * b) / (b - a));
 z = ((Math.Pow((x * x) - 2 * x, 3) - 4 * (Math.Pow(x, 4) + 1)) * (1 - b)) / (5 * a + 3 * b);
-r = (((1 / 2) * a + (3 / 4) * b - 7 / 5) / 3 * c + 1) + 1 / (a - c);
+r = (((1.0 / 2.0) * a + (3.0 / 4.0) * b - 7.0 / 5.0) / 3 * c + 1) + 1 / (a - c);
 
 Console.WriteLine($"Результати обчислень виразів:\n x = {x}\n y = {y}\n z = {z}\n r = {r}");
