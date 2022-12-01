9e6442d6bee84fa40fe07933a4d1ded927e12254
Kuprienko Anatolii <ipz222_kaa@student.ztu.edu.ua>
2022-12-01 13:24:18 +0200

    SanaPrakt3

---
+++ LinearExpressions1/Program.cs
@@ -0,0 +1,14 @@
+﻿double a, b, c, d, x, y, z, r;
+Console.WriteLine("Input the value of a:");
+a = double.Parse(Console.ReadLine());
+Console.WriteLine("Input the value of b:");
+b = double.Parse(Console.ReadLine());
+Console.WriteLine("Input the value of c:");
+c = double.Parse(Console.ReadLine());
+Console.WriteLine("Input the value of d:");
+d = double.Parse(Console.ReadLine());
+x = (a + 2 * b - c + d) / (c * d) + (a + b) / (c - d) - Math.Pow(a, 2) / Math.Pow(b, 2);
+y = ((5 * (a + b) * (c - d)) / ((1.0 / 2) * c)) + (Math.Pow(d, 2) * (Math.Pow(a, 2) - Math.Pow(b, 2)) / (b - a));
+z = (Math.Pow(Math.Pow(x, 2) - 2 * x, 3) - 4 * (Math.Pow(x, 4) + 1) * (1 - b)) / (5 * a + 3 * b);
+r = (((1 / 2.0) * a) + ((3.0 / 4) * b) - 7.0 / 5) / (3 * c + 1) + 1 / (a - c);
+Console.WriteLine($"x = {x}\ny = {y}\nz = {z}\nr = {r}");
