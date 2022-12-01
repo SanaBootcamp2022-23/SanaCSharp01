a94f0bc548d992fbe367b0a80b3a61085d887190
Denis Kyrylovych <117088087+Kryklya@users.noreply.github.com>
2022-12-01 13:11:21 +0200

    Task done

---
+++ LinearExpressions1/Program.cs
@@ -0,0 +1,42 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+
+        Console.Write("а = ");
+
+        double a = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("b = ");
+
+        double b = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("c = ");
+
+        double c = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("d = ");
+
+        double d = double.Parse(Console.ReadLine());
+
+        double x = ((a + (2 * b) - c + d) / (c * d)) + ((a + b) / (c - d)) - (Math.Pow(a, 2) / Math.Pow(b, 2));
+
+        double y = (((5 * (a + b) * (c - d)) / (0.5 * c))) + Math.Pow(d, 2) * ((Math.Pow(a, 2) - Math.Pow(b, 2) / (b - a)));
+
+        double z = (Math.Pow(Math.Pow(x, 2) - (2 * x), 3) - (4 * ((Math.Pow(x, 4) + 1) * (1 - b))) / ((5 * a) + 3 * b));
+
+        double r = (((0.5 * a) + (0.75 * b) - 1.4) / ((3 * c) + 1)) + (1 / (a - c));
+
+        Console.Write("Розрахунок виразів:");
+
+        Console.Write("\nx = " + x);
+
+        Console.Write("\ny = " + y);
+
+        Console.Write("\nz = " + z);
+
+        Console.Write("\nr = " + r);
+
+    }
+}
+
---
+++ LinearExpressions2/Program.cs
@@ -0,0 +1,40 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+
+        Console.Write("m = ");
+
+        double m = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("n = ");
+
+        double n = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("a = ");
+
+        double a = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("b = ");
+
+        double b = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("x = ");
+
+        double x = Convert.ToDouble(Console.ReadLine());
+
+        double z1 = (((m - 1) * Math.Sqrt(m)) - ((n - 1) * Math.Sqrt(n))) / (Math.Sqrt(Math.Pow(m, 3) * n) + (n * m) + Math.Pow(m, 2) - m);
+
+        double z2 = (Math.Sqrt(m) - Math.Sqrt(n)) / m;
+
+        double y = (2.4 * Math.Abs((Math.Pow(x, 2)) + (b)) / (a)) + ((a - b) * (Math.Pow(Math.Sin((a - b) + (Math.Pow(10, -2) * (x - b))), 2)));
+
+        Console.Write("Розрахунок виразів:");
+
+        Console.Write("\nz1 = " + z1);
+
+        Console.Write("\nz2 = " + z2);
+
+        Console.Write("\ny = " + y);
+    }
+}
