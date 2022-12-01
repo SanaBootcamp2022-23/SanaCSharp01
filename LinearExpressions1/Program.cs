7cdc12cbab10eb2a66f77e61856abfed1832a97e
Maria Evdokymova <sana22_yemo@student.ztu.edu.ua>
2022-12-01 23:27:22 +0200

    corrected the errors

--- LinearExpressions1/Program.cs
+++ LinearExpressions1/Program.cs
@@ -21,14 +21,14 @@ c = double.Parse(Console.ReadLine());
 Console.WriteLine("to calculate the formulla, enter the namber d");
 d = double.Parse(Console.ReadLine());
 
-double x = ((a + 2.0 * b - c + d) / c * d) + ((a + b) / (c - d)) - ((a * a) / (b * b));
-Console.WriteLine($"x = ((a + 2 * b - c + d) / c * d) + ((a + b) / (c - d)) - ((a * a) / (b * b)) = { x }");
+double x = ((a + 2.0 * b - c + d) / c * d) + ((a + b) / (c - d)) - (Math.Pow(a, 2) / Math.Pow(b, 2));
+Console.WriteLine($"x = ((a + 2 * b - c + d) / c * d) + ((a + b) / (c - d)) - (Math.Pow(a, 2) / Math.Pow(b, 2)) = { x }");
 
-double y = ((5.0 * (a + b) * (c - d)) / (1.0 / 2.0 * c)) + ((d * d) * (a * a - b * b) / (b - a));
-Console.WriteLine($"y = ((5.0 * (a + b) * (c - d)) / (1.0 / 2.0 * c)) + ((d * d) * ((a * a - b * b) / (b - a)) = { y }");
+double y = ((5.0 * (a + b) * (c - d)) / (1.0 / 2.0 * c)) + (Math.Pow(d, 2) * ((Math.Pow(a, 2) - Math.Pow(b, 2)) / (b - a)));
+Console.WriteLine($"y = ((5.0 * (a + b) * (c - d)) / (1.0 / 2.0 * c)) + (Math.Pow(d, 2) * (Math.Pow(a, 2) - Math.Pow(b, 2) / (b - a)) = { y }");
 
-double z = (((x * x - 2 * x) * (x * x - 2 * x) * (x * x - 2 * x)) - 4 * (x * x * x * x + 1.0)) * (1.0 - b) / (5.0 * a + 3.0 * b);
-Console.WriteLine($"z = (((x * x - 2 * x) * (x * x - 2 * x) * (x * x - 2 * x)) - 4 * (x *x*x*x + 1.0)) * (1.0 - b)/(5.0 * a + 3.0 * b) = { z }");
+double z = ((Math.Pow((Math.Pow(x, 2) - 2 * x), 3) - 4 * (Math.Pow(x, 4) + 1.0)) * (1.0 - b)) / (5.0 * a + 3.0 * b);
+Console.WriteLine($"z = ((Math.Pow((Math.Pow(x, 2) - 2 * x), 3) - 4 * (Math.Pow(x, 4) + 1.0)) * (1.0 - b)) / (5.0 * a + 3.0 * b) = { z }");
 
 double r = ((1.0 / 2.0 * a) + (3.0 / 4.0 * b) - (7 / 5) / (3.0 * c + 1.0)) + (1.0 / (a - c));
 Console.WriteLine($"r = ((1.0 / 2.0 * a) + (3.0 / 4.0 * b) - (7 / 5) / (3.0 * c + 1.0)) + (1.0 / (a - c)) = {r}");
