82091be4b21783f8826254bc85d6974be6db6e87
Serhii Baranovskyi <sergeybaranovskiy2001@gmail.com>
2022-12-01 22:14:07 +0200

    Create project LinearExpressions2

--- SanaCShapr01/LinearExpressions1/Program.cs
+++ SanaCShapr01/LinearExpressions1/Program.cs
@@ -10,10 +10,10 @@ double d = double.Parse(Console.ReadLine());
 double x = ((a + 2 * b - c + d) / (c * d)) + ((a + b) / (c - d)) - ((a * a) / (b * b));
 Console.WriteLine($"x={x}");
 
-double y = ((5 * (a + b) * (c - d)) / (c / 2)) + ((d * d * ((a * a - b * b) / (b - a))));
+double y = ((5 * (a + b) * (c - d)) / (c / 2)) + ((d * d * (((a * a) - (b * b)) / (b - a))));
 Console.WriteLine($"y={y}");
 
-double z = ((Math.Pow((x * x - 2 * x), 3) - 4 * (Math.Pow(x, 4) + 1)) * (1 - b)) / (5 * a + 3 * b);
+double z = ((Math.Pow((x * x - 2 * x), 3) - 4 * (Math.Pow(x, 4) + 1)) * (1 - b)) / ((5 * a) + (3 * b));
 Console.WriteLine($"z={z}");
 
 double r = (((a / 2) + ((3 * b) / 4) - (7 / 5)) / (3 * c + 1)) + (1 / (a - c));
---
+++ SanaCShapr01/LinearExpressions2/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Please, input m:\n");
+double m = double.Parse(Console.ReadLine());
+Console.Write("Please, input n:\n");
+double n = double.Parse(Console.ReadLine());
+Console.Write("Please, input a:\n");
+double a = double.Parse(Console.ReadLine());
+Console.Write("Please, input b:\n");
+double b = double.Parse(Console.ReadLine());
+Console.Write("Please, input x:\n");
+double x = double.Parse(Console.ReadLine());
+
+double z1 = (((m - 1) * (Math.Sqrt(m))) - ((n - 1) * (Math.Sqrt(n)))) / ((Math.Sqrt((Math.Pow(m, 3)) * n) + (m * n) + (m * m) - m));
+Console.WriteLine($"z1={z1}");
+
+double z2 = ((Math.Sqrt(m)) - (Math.Sqrt(n))) / (m);
+Console.WriteLine($"z2={z2}");
+
+double y = (2.4 * Math.Abs((x * x + b) / (a))) + ((a - b) * ((Math.Sin(a - b) * Math.Sin(a - b)))) + (Math.Pow(10, -2) * (x - b));
+Console.WriteLine($"y={y}");
