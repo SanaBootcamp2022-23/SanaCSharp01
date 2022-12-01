f520d74276f83846fdbb9c59b9f6f631a2323b2b
Andrii Nemerovskii <andreynemerovskiy@gmail.com>
2022-12-01 22:49:18 +0200

    Create LinearExpressions2

--- SanaCShapr01/LinearExpressions1/Program.cs
+++ SanaCShapr01/LinearExpressions1/Program.cs
@@ -1,2 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+double x, y, z, r, a, b, c, d;
+Console.WriteLine("Enter variable values:");
+Console.Write("a = "); a = double.Parse(Console.ReadLine());
+Console.Write("b = "); b = double.Parse(Console.ReadLine());
+Console.Write("c = "); c = double.Parse(Console.ReadLine());
+Console.Write("d = "); d = double.Parse(Console.ReadLine());
+
+Console.WriteLine("\nx = ((a + 2 * b - c + d) / (c * d)) + ((a + b) / (c - d)) - ((a * a) / (b * b))");    
+x = ((a + 2 * b - c + d) / (c * d)) + ((a + b) / (c - d)) - ((a * a) / (b * b));
+Console.WriteLine($"x = {x}\n");
+
+Console.WriteLine("y = ((5 * (a + b) * (c - d)) / (0.5 * c)) + (d * d) * (((a * a) - (b * b)) / (b - a))");
+y = ((5 * (a + b) * (c - d)) / (0.5 * c)) + (d * d) * (((a * a) - (b * b)) / (b - a));
+Console.WriteLine($"y = {y}\n");
+
+Console.Write("Enter value x = "); x = double.Parse(Console.ReadLine());
+Console.WriteLine("\nz = ((Math.Pow(x * x - 2 * x, 3) - 4 * (Math.Pow(x, 4) + 1) * (1 - b)) / (5 * a + 3 * b))");
+z = ((Math.Pow(x * x - 2 * x, 3) - 4 * (Math.Pow(x, 4) + 1) * (1 - b)) / (5 * a + 3 * b));
+Console.WriteLine($"z = {z}\n");
+
+Console.WriteLine("r = (((0.5 * a + 0.75 * b - 1.4) / (3 * c) + 1) + (1 / (a - c)))");
+r = (((0.5 * a + 0.75 * b - 1.4) / ((3 * c) + 1)) + (1 / (a - c)));
+Console.WriteLine($"r = {r}");
