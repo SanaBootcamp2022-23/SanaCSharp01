bb022a8b1e0388765934f2ed149a1ccf9098bd09
Denis Yakovenko <kn202_yadv@student.ztu.edu.ua>
2022-12-01 09:15:21 +0200

    Реалiзовано обрахунок значення (z1)

--- SanaCShapr01/LinearExpressions2/Program.cs
+++ SanaCShapr01/LinearExpressions2/Program.cs
@@ -1,6 +1,8 @@
-﻿double a, b, x, m, n, z1, z2, y;
+﻿using static System.Math;
 
-Console.Write("Enter value a: ");
+double a, b, x, m, n, z1, z2, y;
+
+/*Console.Write("Enter value a: ");
 a = double.Parse(Console.ReadLine());
 
 Console.Write("Enter value b: ");
@@ -14,4 +16,14 @@ m = double.Parse(Console.ReadLine());
 
 Console.Write("Enter value n: ");
 n = double.Parse(Console.ReadLine());
+*/
 
+a = 1;
+b = 2;
+x = 3;
+m = 4;
+n = 5;
+
+z1 = ((m - 1) * Sqrt(m) - (n - 1) * Sqrt(n))
+    / (Sqrt(Pow(m, 3) * n) + (n * m) + Pow(m, 2) - m);
+Console.WriteLine("z1 = {0:F3}", z1);
