0152de946067533983e723b5bfc82dc841764ea9
Maria Evdokymova <sana22_yemo@student.ztu.edu.ua>
2022-12-01 23:37:04 +0200

    corrected the errors

--- LinearExpressions2/Program.cs
+++ LinearExpressions2/Program.cs
@@ -27,12 +27,13 @@ Console.WriteLine("to calculate the formulla? entr the namber x");
 x = double.Parse(Console.ReadLine());
 
 double z1 = ((m - 1.0) * Math.Sqrt(m)) - ((n - 1) * Math.Sqrt(n)) / Math.Sqrt(Math.Pow(m, 3) * n) + n * m + Math.Pow(m, 2) - m;
-Console.WriteLine($"z1 = ((m - 1.0) * Math.Sqrt(m)) - ((n - 1) * Math.Sqrt(n)) / Math.Sqrt(Math.Pow(m, 3) * n) + n * m + Math.Pow(m, 2) - m = {z1}");
+Console.WriteLine($"z1 = ((m - 1.0) * Math.Sqrt(m)) - ((n - 1) * Math.Sqrt(n)) / Math.Sqrt(Math.Pow(m, 3) * n) + n * m + Math.Pow(m, 2) - m = { z1 }");
 
 double z2 = (Math.Sqrt(m) - Math.Sqrt(n))/ m;
-Console.WriteLine($"z2 = (Math.Sqrt(m) - Math.Sqrt(n))/ m = {z2}");
+Console.WriteLine($"z2 = (Math.Sqrt(m) - Math.Sqrt(n))/ m = { z2 }");
 
 double y = 2.4 * Math.Abs((Math.Pow(x, 2) + b / a)) +Math.Pow((a - b), 2) * Math.Pow(Math.Sin(1), 2) + Math.Pow(10, -2) * (x - b);
+Console.WriteLine($"y = (Math.Sqrt(m) - Math.Sqrt(n))/ m = { y }");
 
 
 Console.ReadKey();
