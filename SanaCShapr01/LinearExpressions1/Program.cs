733978a868b8895a126ad712a2412aa52c0230a2
Olena Halaiko <lenagalayko66@gmail.com>
2022-12-01 20:47:29 +0200

    change LinearExp1, add LinearExp2

--- SanaCShapr01/LinearExpressions1/Program.cs
+++ SanaCShapr01/LinearExpressions1/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double a, b, c, d;
+
+Console.WriteLine("Enter number 'а':");
+a = double.Parse(Console.ReadLine());
+Console.WriteLine("Enter number 'b':");
+b = double.Parse(Console.ReadLine());
+Console.WriteLine("Enter number 'c':");
+c = double.Parse(Console.ReadLine());
+Console.WriteLine("Enter number 'd':");
+d = double.Parse(Console.ReadLine());
+
+double x = ((a + 2 * b - c + d) / (c * d)) + ((a + b) / (c - d)) - ((Math.Pow(a, 2)) / Math.Pow(b, 2));
+double y = ((5 * (a + b) * (c - d)) / ((1 / 2.0) * c)) + (Math.Pow(d,2)) * (Math.Pow(a,2) - Math.Pow(b,2))/(b-a);
+double z = ((Math.Pow((Math.Pow(x,2) - 2 * x),3) - 4 * (Math.Pow(x,4) + 1)) * (1-b)) / (5 * a + 3 * b);
+double r = (((1/2.0 * a) + 3/4.0*b - 7/5.0) / (3*c + 1)) + (1 / (a - c));
+
+Console.WriteLine($"Result x = {x}");
+Console.WriteLine($"Result y = {y}");
+Console.WriteLine($"Result z = {z}");
+Console.WriteLine($"Result r = {r}");
