edae23a5e51267e924f149c1f7ca776aff184f36
Andrii Gras <sana22_gav@student.ztu.edu.ua>
2022-12-01 12:48:03 +0200

    project updated

---
+++ LinearExpressions1/LinearExpressions1/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+double a, b, c, d;
+Console.WriteLine("Input a");
+a = float.Parse(Console.ReadLine());
+Console.WriteLine("Input b");
+b = float.Parse(Console.ReadLine());
+Console.WriteLine("Input c");
+c = float.Parse(Console.ReadLine());
+Console.WriteLine("Input c");
+d = float.Parse(Console.ReadLine());
+double x,y,z,r;
+z = (a + Math.Pow( a, 2) - c + d / (c * d)) + (a + b) / (c - d) - Math.Pow(a, 2) / Math.Pow(b, 2);
+Console.WriteLine(z);
