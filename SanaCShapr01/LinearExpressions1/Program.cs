d726ba0c108b5cda2224935842a4a71e10712374
Walenty Sawicki <sana22_vsb@student.ztu.edu.ua>
2022-12-01 17:32:53 +0100

    Modified LinearExpressions1

--- SanaCShapr01/LinearExpressions1/Program.cs
+++ SanaCShapr01/LinearExpressions1/Program.cs
@@ -13,10 +13,10 @@ double c = double.Parse(Console.ReadLine());
 Console.Write("Enter d: ");
 double d = double.Parse(Console.ReadLine());
 x = (((a + (2 * b) - c + d) / (c * d)) + ((a + b) / (c - d)) - 
-    ((Math.Sqrt(a)) / (Math.Sqrt(b))));
+    ((Math.Pow(a, 2)) / (Math.Pow(b, 2))));
 y = (((5 * (a + b) * (c - d)) / (0.5 * c)) +
-    (Math.Sqrt(d) * ((Math.Sqrt(a) - Math.Sqrt(b)) / (b - a))));
-z = ((((Math.Pow((Math.Sqrt(x) - 2 * x), 3)) - 
+    (Math.Pow(d, 2) * ((Math.Pow(a, 2) - Math.Pow(b, 2)) / (b - a))));
+z = ((((Math.Pow((Math.Pow(x, 2) - 2 * x), 3)) - 
     (4*(Math.Pow(x, 4) + 1)))*(1 - b))/(5*a + 3*b));
 r = (((0.5*a + 0.75*b - 1.4)/(3*c + 1)) + (1/(a - c)));
 Console.WriteLine("\nRESULTS:\nValue x = " + x);
