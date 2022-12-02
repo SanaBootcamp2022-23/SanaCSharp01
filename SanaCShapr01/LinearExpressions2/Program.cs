897793807d2a593bee7c22b372785e3f8fc9e78b
Igor Ostrohliad <sana22_oid@student.ztu.edu.ua>
2022-12-02 17:47:20 +0300

    Done

---
+++ SanaCShapr01/LinearExpressions2/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//введення, оголошення та перетворення змінних 
+Console.WriteLine("Input m:");
+double m = double.Parse(Console.ReadLine());
+Console.WriteLine("Input n:");
+double n = double.Parse(Console.ReadLine());
+
+//обчислення виразів
+double z1 = ((m - 1) * Math.Sqrt(m) - (n - 1) * Math.Sqrt(n)) / (Math.Sqrt(Math.Pow(m, 3) * n) + n * m + Math.Pow(m, 2) - m);
+double z2 = (Math.Sqrt(m) - Math.Sqrt(n)) / m;
+
+//виведення результату 1 та 2 виразу
+Console.WriteLine($"\nz1 = {z1}, z2 = {z2}\n");
+
+//введення, оголошення та перетворення змінних 
+Console.WriteLine("Input a:");
+double a = double.Parse(Console.ReadLine());
+Console.WriteLine("Input b:");
+double b = double.Parse(Console.ReadLine());
+Console.WriteLine("Input x:");
+double x = double.Parse(Console.ReadLine());
+
+//обчислення виразу
+double y = 2.4 * Math.Abs((Math.Pow(x, 2) + b) / a) + (a - b) * Math.Pow(Math.Sin(a - b), 2) + Math.Pow(10, -2) * (x - b);
+
+//виведення результату 3го виразу
+Console.WriteLine($"\ny = {y}");
+
