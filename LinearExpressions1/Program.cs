0092908604f64d42f44c8f058a189b626fc3ca76
VitaliiTushak <ipz224_tvs@student.ztu.edu.ua>
2022-12-02 16:26:53 +0200

    SanaCSharp01

---
+++ LinearExpressions1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SanaCSharp01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = UTF8Encoding.UTF8;
+            Console.WriteLine(" Обчислення виразів");
+            double a, b, c, d;
+            Console.Write(" Введіть значення змінної a:");
+            a = double.Parse(Console.ReadLine());
+            Console.Write(" Введіть значення змінної b:");
+            b = double.Parse(Console.ReadLine());
+            Console.Write(" Введіть значення змінної c:");
+            c = double.Parse(Console.ReadLine());
+            Console.Write(" Введіть значення змінної d:");
+            d = double.Parse(Console.ReadLine());
+
+            double x, y, z, r;
+            x = (a + 2 * b - c + d) / (c * d) + (a + b) / (c - d) - (a * a) / (b * b);
+            y = (5 * (a + b) * (c - d)) / (1 / 2.0 * c) + d * d * ((a * a - b * b) / (b - a));
+            z = ((Math.Pow(x * x - 2 * x, 3) - 4*((Math.Pow(x, 4) + 1) * (1 - b)))) / (5 * a + 3 * b);
+            r = (1 / 2.0 * a + 3 / 4.0 * b - 7 / 5.0) / (3 * c + 1) + 1 / (a - c);
+
+            Console.WriteLine($" x = {x}\n y = {y}\n z = {z}\n r = {r}");
+        }
+    }
+}
---
+++ LinearExpressions2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SanaCSharp01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = UTF8Encoding.UTF8;
+            Console.WriteLine(" Обчислення виразів");
+            double a, b, m, n, x;
+            Console.Write(" Введіть значення змінної m:");
+            m = double.Parse(Console.ReadLine());
+            Console.Write(" Введіть значення змінної n:");
+            n = double.Parse(Console.ReadLine());
+            Console.Write(" Введіть значення змінної a:");
+            a = double.Parse(Console.ReadLine());
+            Console.Write(" Введіть значення змінної b:");
+            b = double.Parse(Console.ReadLine());
+            Console.Write(" Введіть значення змінної x:");
+            x = double.Parse(Console.ReadLine());
+
+            double z1, z2, y; //result
+
+            z1 = ((m - 1) * Math.Sqrt(m) - (n - 1) * Math.Sqrt(n)) / (Math.Sqrt(Math.Pow(m, 3) * n) + n * m + m * m - m);
+            z2 = (Math.Sqrt(m) - Math.Sqrt(n)) / m;
+            y = 2.4 * Math.Abs((x * x + b) / a) + (a - b) * (Math.Pow(Math.Sin(a - b), 2)) + Math.Pow(10, -2) * (x - b);
+            
+            Console.WriteLine($" \nz1 = {z1}\n z2 = {z2}\n y = {y}\n ");
+        }
+    }
+}
