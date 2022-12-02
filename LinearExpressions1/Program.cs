f8142add00ea18e1a380b5889c181fe0b04fd197
nikotsim <tsimbalyuk88@gmail.com>
2022-12-02 13:33:12 +0200

    created linear expressions calc v.2.0

--- LinearExpressions1/Program.cs
+++ LinearExpressions1/Program.cs
@@ -17,15 +17,12 @@ Console.WriteLine("b = " + b);
 Console.WriteLine("c = " + c);
 Console.WriteLine("d = " + d);
 Console.WriteLine("- - - - - - - - - - - - -");
-
-Console.WriteLine("Результати виконання формул: ");
 x = ((a + 2*b - c + d)/(c*d)) + ((a + b)/(c - d)) - ((a*a)/(b*b));
-Console.WriteLine("x = " + x);
-
 y = ((5*(a + b)*(c - d))/((1/2)*c)) + ((d*d)*(((a*a) - (b*b))/(b - a)));
-Console.WriteLine("y = " + y);
-//y = (5 * (a + b) * (c - d)) / ((1 / 2) * c) + Math.Pow(d, 2) * ((Math.Pow(a, 2) - Math.Pow(b, 2)) / (b - a));
 z = (((Math.Pow((x*x - 2*x),3)) - 4*(Math.Pow(x,4) + 1))*(1 - b)) / (5*a + 3*b);
-Console.WriteLine("z = " + z);
 r = ((((1/2)*a)+((3/4)*b)-(7/5))/(3*c + 1)) + (1/(a - c));
+Console.WriteLine("Результати виконання формул: ");
+Console.WriteLine("x = " + x);
+Console.WriteLine("y = " + y);
+Console.WriteLine("z = " + z);
 Console.WriteLine("r = " + r);
