f19c67e78bd8aca5c8c51e6f5c63d96134e6030f
Аліхан Бекіров <alikhanbekirov@gmail.com>
2022-12-02 13:42:09 +0200

    SanaCShapr01

---
+++ SanaCShapr01/LinearExpressions1/Program.cs
@@ -0,0 +1,19 @@
+﻿double a, b, c, d,x,y,z,r;
+Console.WriteLine("Введіть а");
+a=Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введіть b");
+b = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введіть c");
+c = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введіть d");
+d = Convert.ToDouble(Console.ReadLine());
+x = ((a + 2 * b - c + d) / (c * d)) + ((a + b) / (c - d)) - ((a * a) / (b * b));
+y = ((5 * (a + b) * (c - d)) / (1.0 / 2 * c)) + (d * d) * (Math.Pow(a, 2) - Math.Pow(b, 2)) / (b - a);
+z = ((Math.Pow((x * x) - (2 * x), 3) - 4 * (Math.Pow(x, 4) + 1)) * (1 - b)) / (5 * a + 3 * b);
+r = ((1.0 / 2 * a + 3.0 / 4 * b - 7.0 / 6) / (3 * c + 1)) + 1 / (a - c);
+Console.WriteLine("x="+x);
+Console.WriteLine("y="+y);
+Console.WriteLine("z="+z);
+Console.WriteLine("r="+r);
+
+
