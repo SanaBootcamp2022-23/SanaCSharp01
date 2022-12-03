ed5e4eca641b9285cc6e3630f46f0622e4d1ea5e
AdamLahovskyi <ipz221_lav@student.ztu.edu.ua>
2022-12-03 15:14:27 +0200

    SanaPrakt03

---
+++ LinearExpressions2/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+double z1, z2, m, n, x, b, a, y, t1,t2;
+
+Console.Write("Input 'm' value: "); m = Convert.ToDouble((Console.ReadLine()));
+Console.Write("Input 'n' value: "); n = Convert.ToDouble((Console.ReadLine()));
+Console.Write("Input 'x' value: "); x = Convert.ToDouble((Console.ReadLine()));
+Console.Write("Input 'b' value: "); b = Convert.ToDouble((Console.ReadLine()));
+Console.Write("Input 'a' value: "); a = Convert.ToDouble((Console.ReadLine()));
+
+z1 = (Math.Sqrt(m) * (m - 1) - Math.Sqrt(n) * (n - 1)) / (Math.Sqrt(Math.Pow(3, m) * n) + n * m + m * m - m);
+z2 = (Math.Sqrt(m) - Math.Sqrt(n)) / m;
+y = 2.4 * Math.Abs((x * x + b) / a) + Math.Sin(a - b) * (a - b) + Math.Pow(-2, 10) * (x - b);
+
+Console.WriteLine("Z1 equals " + z1);
+Console.WriteLine("Z2 equals " + z2);
+Console.WriteLine("Y equals " + y);
+
+
+
+
