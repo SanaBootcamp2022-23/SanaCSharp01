9e9b0088d36d89b242ba6426b5912fd1be3807ce
ipz222_tmo <ipz222_tmo@student.ztu.edu.ua>
2022-12-03 18:10:08 +0200

    Created 2 programs.cs

---
+++ LinearExpressions1/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+double a, b, c, d;
+Console.WriteLine("Enter variable a:");
+a = double.Parse(Console.ReadLine());
+Console.WriteLine("Enter variable b:");
+b = double.Parse(Console.ReadLine());
+Console.WriteLine("Enter variable c:");
+c = double.Parse(Console.ReadLine());
+Console.WriteLine("Enter variable d:");
+d = double.Parse(Console.ReadLine());
+double x = (a + 2 * b - c + d) / (c * d) + (a + b) / (c - d) - (a * a) / (b * b);
+double y = (5 * (a + b) * (c - d)) / (1 / 2 * c) + (d * d) * ((a * a - b * b) / (b - a));
+double z = ((Math.Pow((x * x - 2 * x), 3) - 4 * (x * x * x * x + 1)) * (1 - b)) / (5 * a + 3 * b);
+double r = (1 / 2 * a + 3 / 4 * b - 7 / 5) / (3 * c + 1) + 1 / (a - c);
+Console.WriteLine($"Result x = {x}");
+Console.WriteLine($"Result y = {y}");
+Console.WriteLine($"Result z = {z}");
+Console.WriteLine($"Result r = {r}");
---
+++ LinearExpressions2/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+double m, n;
+Console.WriteLine("Enter variable m:");
+m = double.Parse(Console.ReadLine());
+Console.WriteLine("Enter variable n:");
+n = double.Parse(Console.ReadLine());
+double z1 = ((m-1)*Math.Sqrt(m)-(n-1)*Math.Sqrt(n))/((Math.Sqrt(Math.Pow(m, 3)*n))+n*m+Math.Pow(m,2)-m);
+double z2 = (Math.Sqrt(m) - Math.Sqrt(n)) / m;
+Console.WriteLine($"Result z1 = {z1}");
+Console.WriteLine($"Result z2 = {z2}");
+double a, b, x;
+Console.WriteLine("Enter variable a:");
+a = double.Parse(Console.ReadLine());
+Console.WriteLine("Enter variable b:");
+b= double.Parse(Console.ReadLine());
+Console.WriteLine("Enter variable x:");
+x = double.Parse(Console.ReadLine());
+double y = 2.4 * Math.Abs((x * x + b) / a) + (a - b) * Math.Pow(Math.Sin(a - b), 2) + Math.Pow(10, -2) * (x - b);
+Console.WriteLine($"Result y = {y}");
