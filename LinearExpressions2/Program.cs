2435d0446335d980acf5af7454fb1ea0285b57c1
Bohdan Andriichuk <sana22_abv@student.ztu.edu.ua>
2022-12-03 15:25:39 +0200

    Program

---
+++ LinearExpressions2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("m=");
+double m = double.Parse(Console.ReadLine());
+Console.Write("n=");
+double n = double.Parse(Console.ReadLine());
+Console.Write("a=");
+double a = double.Parse(Console.ReadLine());
+Console.Write("b=");
+double b = double.Parse(Console.ReadLine());
+Console.Write("x=");
+double x = double.Parse(Console.ReadLine());
+
+
+double z1 = ((m - 1) * Math.Sqrt(m) - (n - 1) * Math.Sqrt(n)) / (Math.Sqrt(m * m * m * n) / (n * m) / (m * m) - m);
+double z2 = (Math.Sqrt(m) - Math.Sqrt(n)) / m;
+double y = 2.4 * Math.Abs((x * x + b) / a) + (a - b) * Math.Pow(Math.Sin(a - b), 2) + 1 / 100 * (x - b);
+Console.WriteLine($"z1={z1}\nz2={z2}\ny={y}");
