3a7d3a41a7994821002ff8e34792c49f8065ab77
Renata Smolska <sana22_srs@student.ztu.edu.ua>
2022-12-04 01:56:58 +0200

    Created 2 projects

---
+++ SanaCShapr01/LinearExpressions1/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.WriteLine("Input number a");
+double a;
+a = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Input number b");
+double b;
+b = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Input number c");
+double c;
+c = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Input number d");
+double d;
+d = double.Parse(Console.ReadLine());
+
+double x;
+x = ((a + 2.0 * b - c + d) / (c * d)) + ((a + b) / (c - d)) - (Math.Pow(a, 2) / Math.Pow(b, 2));
+Console.WriteLine($"Result of calculating x: {x}");
+
+double y;
+y = ((5.0 * (a +  b) * (c - d)) / (1.0 / 2.0 * c)) + (Math.Pow(d, 2) * ((Math.Pow(a, 2) - Math.Pow(b, 2)) / (b - a)));
+Console.WriteLine($"Result of calculating y: {y}");
+
+double z;
+z = ((Math.Pow((Math.Pow(x, 2) - 2.0 * x), 3) - 4.0 * (Math.Pow(x, 4) + 1.0)) * (1.0 - b)) / (5.0 * a + 3.0 * b);
+Console.WriteLine($"Result of calculating z: {z}");
+
+double r; 
+r = ((1.0 / 2.0 * a + 3.0 / 4.0 * b - 7.0 / 5.0) / (3.0 * c + 1.0)) + (1.0 / (a - c));
+Console.WriteLine($"Result of calculating r: {r}");
+
+
---
+++ SanaCShapr01/LinearExpressions2/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.WriteLine("Input number m");
+double m;
+m = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Input number n");
+double n;
+n = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Input number a");
+double a;
+a = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Input number b");
+double b;
+b = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Input number x");
+double x;
+x = double.Parse(Console.ReadLine());
+
+double z1;
+z1 = ((m - 1.0) * Math.Sqrt(m) - (n - 1.0) * Math.Sqrt(n)) / (Math.Sqrt(Math.Pow(m, 3) * n) + n * m + Math.Pow(m, 2) - m);
+Console.WriteLine($"Result of calculating z1: {z1}");
+
+double z2;
+z2 = (Math.Sqrt(m) - Math.Sqrt(n)) / m;
+Console.WriteLine($"Result of calculating z2: {z2}");
+
+double y;
+y = 2.4 * Math.Abs((Math.Pow(x, 2) + b) / a) + (a - b) * Math.Pow(Math.Sin(a - b), 2) + Math.Pow(10, -2) * (x - b);
+Console.WriteLine($"Result of calculating y: {y}");
+
+
+
