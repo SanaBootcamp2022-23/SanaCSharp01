13a32a17de1851f2fe3cb07a2d297269416005ec
SashaZaitsev <ipz224_zoi@student.ztu.edu.ua>
2022-12-03 20:17:48 +0200

    Lab03

---
+++ SanaCShapr01/LinearExpressions1/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("a: ");
+double a = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("b: ");
+double b = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("c: ");
+double c = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("d: ");
+double d = Convert.ToDouble(Console.ReadLine());
+
+double x = ((a + 2 * b - c + d) / (c * d)) + ((a + b) / (c - d)) - ((a * a) / (b * b));
+
+double y = ((5 * (a + b) * (c - d)) / (c / 2)) + ((d * d * (a * a - b * b)) / (b - a));
+
+double z = (Math.Pow(x * x - 2 * x, 3) - 4 * (x * x * x * x + 1) * (1 - b)) / (5 * a + 3 * b);
+
+double r = (a / 2 + 3 * b / 4.0 - 1.4) / (3 * c + 1) + (1 / (a - c));
+
+Console.WriteLine("---Results---");
+Console.WriteLine("x: " + x);
+Console.WriteLine("y: " + y);
+Console.WriteLine("z: " + z);
+Console.WriteLine("r: " + r);
---
+++ SanaCShapr01/LinearExpressions2/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("m: ");
+double m = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("n: ");
+double n = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("x: ");
+double x = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("a: ");
+double a = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("b: ");
+double b = Convert.ToDouble(Console.ReadLine());
+
+double z1 = ((m - 1) * Math.Sqrt(m) - (n - 1) * Math.Sqrt(n)) / (Math.Sqrt(n * m * m * m) + n * m + m * m - m);
+double z2 = (Math.Sqrt(m) - Math.Sqrt(n)) / m;
+double y = 2.4 * Math.Abs((x * x + b) / a) + (a - b) * Math.Sin(a - b) * Math.Sin(a - b) + 0.01 * (x - b);
+
+Console.WriteLine("---Results---");
+
+Console.WriteLine("z1: " + z1);
+Console.WriteLine("z2: " + z2);
+Console.WriteLine("y: " + y);
