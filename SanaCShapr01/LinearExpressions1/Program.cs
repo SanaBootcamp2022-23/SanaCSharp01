599077f3a70f89ea9b6dd32a80198f9d04e4e41d
Viktor Yelenetskyi <ipz223_yevv@student.ztu.edu.ua>
2022-12-03 23:59:09 +0200

    Done

--- SanaCShapr01/LinearExpressions1/Program.cs
+++ SanaCShapr01/LinearExpressions1/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double a, b, c, d, x, y, z, r;
+
+Console.WriteLine("Enter a = ");
+a = double.Parse(Console.ReadLine());
+Console.WriteLine("Enter b = ");
+b = double.Parse(Console.ReadLine());
+Console.WriteLine("Enter c = ");
+c = double.Parse(Console.ReadLine());
+Console.WriteLine("Enter d = ");
+d = double.Parse(Console.ReadLine());
+
+x = (a + 2 * b - c + d) / (c * d) + (a + b) / (c - d) - Math.Pow(a, 2) / Math.Pow(b, 2);
+Console.WriteLine($"Results: x = {x}");
+y = (5 * (a + b) * (c - d)) / (1.0 / 2 * c) + Math.Pow(d, 2) * ((Math.Pow(a, 2) - Math.Pow(b, 2))) / (b - a);
+Console.WriteLine($"Results: y = {y}");
+z = ((Math.Pow((Math.Pow(x, 2) - 2 * x), 3) - 4 * (Math.Pow(x, 4) + 1)) * (1 - b)) / (5 * a + 3 * b);
+Console.WriteLine($"Results: z = {z}");
+r = (1.0 / 2 * a + 3.0 / 4 * b - 7.0 / 5) / (3 * c + 1) + 1 / (a - c);
+Console.WriteLine($"Results: r = {r}");
+
+
+
--- SanaCShapr01/LinearExpressions2/Program.cs
+++ SanaCShapr01/LinearExpressions2/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double m, n, z1, z2, y, a, b, x;
+
+Console.WriteLine("Enter m = ");
+m = double.Parse(Console.ReadLine());
+Console.WriteLine("Enter n = ");
+n = double.Parse(Console.ReadLine());
+Console.WriteLine("Enter x = ");
+x = double.Parse(Console.ReadLine());
+Console.WriteLine("Enter a = ");
+a = double.Parse(Console.ReadLine());
+Console.WriteLine("Enter b = ");
+b = double.Parse(Console.ReadLine());
+
+z1 = ((m - 1) * Math.Sqrt(m) - (n - 1) * Math.Sqrt(n)) / (Math.Sqrt(Math.Pow(m, 3) * n) + n * m + Math.Pow(m, 2) - m);
+Console.WriteLine("\nResults: z1 = {0}", z1);
+z2 = (Math.Sqrt(m) - Math.Sqrt(n)) / m;
+Console.WriteLine("Results: z2 = {0}", z2);
+y = 2.4 * Math.Abs((Math.Pow(x, 2) + b) / a) + (a - b) * Math.Pow(Math.Sin(a - b), 2) + (Math.Pow(10, -2) * (x - b));
+Console.WriteLine("Results: y = {0}", y);
