4528b1cf68a214162663d0c1749a4871597f689e
Andrew Vozniuk <ipz224_vav@student.ztu.edu.ua>
2022-12-04 07:10:34 +0200

    Initial commit

---
+++ SanaCShapr01/LinearExpressions1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace linearExpressions1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c, d;
+            Console.WriteLine("Entyer the value of a: ");
+            a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Entyer the value of a: ");
+            b = double.Parse(Console.ReadLine());
+            Console.WriteLine("Entyer the value of a: ");
+            c = double.Parse(Console.ReadLine());
+            Console.WriteLine("Entyer the value of a: ");
+            d = double.Parse(Console.ReadLine());
+
+            double x = (a + 2 * b - c + d) / (c * d) + (a + b) / (c - d) - (a * a) / (b * b);
+            Console.WriteLine($"X = {x}");
+            double y = 10 * (a + b) * (c - d) / c + d * d * (a * a - b * b) / (b - a);
+            Console.WriteLine($"Y = {y}");
+            double z = (Math.Pow(x * x - 2 * x, 3) - 4 * (Math.Pow(x, 4) + 1)) * (1 - b) / (5 * a + 3 * b);
+            Console.WriteLine($"Z = {z}");
+            double r = (0.5 * a + (3 / 4.0) * b - 7 / 5.0) / (3 * c + 1) + 1 / (a - c);
+            Console.WriteLine($"R = {r}");
+
+        }
+    }
+}
---
+++ SanaCShapr01/LinearExpressions2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace linearExpressions2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double m, n;
+            Console.WriteLine("Entyer the value of m: ");
+            m = double.Parse(Console.ReadLine());
+            Console.WriteLine("Entyer the value of n: ");
+            n = double.Parse(Console.ReadLine());
+
+            double z1 = ((m - 1) * Math.Sqrt(m) - (n - 1) * Math.Sqrt(n)) / (Math.Sqrt(m * m * m * n) + n * m * m * m - m);
+            Console.WriteLine($"Z1 = {z1}");
+            double z2 = (Math.Sqrt(m) - Math.Sqrt(n)) / m;
+            Console.WriteLine($"Z2 = {z2}");
+
+            double x, a, b;
+            Console.WriteLine("Entyer the value of x: ");
+            x = double.Parse(Console.ReadLine());
+            Console.WriteLine("Entyer the value of a: ");
+            a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Entyer the value of b: ");
+            b = double.Parse(Console.ReadLine());
+
+            double y = 2.4 * Math.Abs((x * x + b) / a) + (a - b) * Math.Pow(Math.Sin(a - b), 2) + (x - b) / 100;
+            Console.WriteLine($"Y = {y}");
+
+        }
+    }
+}
