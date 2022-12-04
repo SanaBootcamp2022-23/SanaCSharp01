9cf30c520a916f345a6e6326a324ad3faa368c68
Oleksandr Parkhomenko <ipzm222_pol@student.ztu.edu.ua>
2022-12-04 11:01:54 +0200

    Expressions 1 & 2

---
+++ LinearExpressions1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.Runtime.CompilerServices;
+
+double a, b, c, d;
+
+Console.WriteLine("Hello, please, enter linear data for computing expressions");
+
+a = enterData("a=");
+b = enterData("b=");
+
+c = enterData("c=");
+d = enterData("d=");
+
+outResult("x = ", computeX(a, b, c, d));
+outResult("y = ", computeY(a, b, c, d));
+outResult("z = ", computeZ(a, b, computeX(a,b,c,d)));
+outResult("r = ", computeR(a, b, c));
+
+
+double computeX(double a, double b, double c, double d)
+{
+    double firstPart = (a + 2 * b - c + d) / c * d;
+    double secondPart = (a + b) / (c - d);
+    double thirdPart = Math.Pow(a,2) / Math.Pow(b,2);
+    return firstPart + secondPart - thirdPart;
+}
+
+double computeY(double a, double b, double c, double d)
+{
+    double firstPart = (5 * (a + b) *  (c - d))/ (c / 2);
+    double secondPart = (Math.Pow(d,2) * (Math.Pow(a,2) - Math.Pow(b,2)))/(b - a);
+    return firstPart + secondPart;
+}
+
+double computeZ(double a, double b, double x)
+{
+    return (Math.Pow((Math.Pow(x, 2) - 2 * x), 3) - 4 * (Math.Pow(x, 4) + 1) * (1 - b)) /
+        (5 * a + 3 * b ); 
+}
+
+double computeR(double a, double b, double c)
+{
+    double firstPart = (a / 2 + (3 * b / 4) - (7 / 5))/(3 * c + 1);
+    double secondPart = 1 / (a - c);
+    return firstPart + secondPart;
+}
+
+void outResult(string s, double x)
+{
+    Console.WriteLine(s + x);
+}
+
+
+double enterData(String s)
+{
+    
+    Console.WriteLine(s);
+    string consoleString = Console.ReadLine();
+    return double.Parse(consoleString);
+}
---
+++ LinearExpressions2/Program.cs
@@ -0,0 +1,48 @@
+﻿double a, b, m, n, x;
+
+Console.WriteLine("Hello, please, enter linear data for computing expressions");
+
+a = enterData("a =");
+b = enterData("b =");
+x = enterData("x =");
+
+m = enterData("m =");
+n = enterData("n =");
+
+outResult("z1 = ", computeZ1(m,n));
+outResult("z2 = ", computeZ2(m,n));
+outResult("y = ", computeY(a,b,x));
+
+
+
+double computeY(double a, double b, double x)
+{
+    double firstPart = 2.4 * Math.Abs((Math.Pow(x,2) + b)/a);
+    double secondPart = (a - b) / Math.Pow(Math.Sin(a - b),2);
+    double thirdPart = (x - b) * Math.Pow(10,-2);
+    return firstPart + secondPart + thirdPart;
+}
+
+double computeZ1(double m, double n)
+{
+    return ((m - 1) * Math.Sqrt(m) - (n - 1) * Math.Sqrt(n)) / 
+        (Math.Sqrt(Math.Pow(m,3) * n) + n * m + Math.Pow(m,2) - m);
+}
+double computeZ2(double m, double n)
+{
+    return (Math.Sqrt(m) - Math.Sqrt(n)) / m;
+}
+
+
+void outResult(string s, double x)
+{
+    Console.WriteLine(s + x);
+}
+
+
+double enterData(String s)
+{
+    Console.WriteLine(s);
+    string consoleString = Console.ReadLine();
+    return double.Parse(consoleString);
+}
