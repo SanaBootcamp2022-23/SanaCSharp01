25f97503e469e1855b5e73bd4b96511f791ed8a2
Novikovsyi Oleksii <sana22_nov@student.ztu.edu.ua>
2022-12-04 22:11:23 +0200

    Novikovsyi Oleksii <sana22_nov@student.ztu.edu.ua>

---
+++ SanaCShapr01/LinearExpressions2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Text;
+double m, n, x, b, a;
+Console.OutputEncoding = UTF8Encoding.UTF8;
+Console.Write("Введення користувачем чотирьох чисел типу double: m, n, x, b, a\nВведіть число m = ");
+m = double.Parse(Console.ReadLine());
+Console.Write("Введіть число n = ");
+n = double.Parse(Console.ReadLine());
+Console.Write("Введіть число x = ");
+x = double.Parse(Console.ReadLine());
+Console.Write("Введіть число b = ");
+b = double.Parse(Console.ReadLine());
+Console.Write("Введіть число a = ");
+a = double.Parse(Console.ReadLine());
+Console.WriteLine($"z1 = " + (((m - 1) * Math.Sqrt(m) - (n - 1) * Math.Sqrt(n)) / (Math.Sqrt(Math.Pow(m, 3) * n) + n * m + Math.Pow(m, 2) - m)).ToString());
+Console.WriteLine($"z2 = " + ((Math.Sqrt(m) - Math.Sqrt(n)) / m).ToString());
+Console.WriteLine($"y = " + (2.4 * Math.Abs((Math.Pow(x, 2) + b) / a) + (a - b) * Math.Pow(Math.Sin(a - b), 2) + Math.Pow(10, -2) * (x - b)).ToString());
