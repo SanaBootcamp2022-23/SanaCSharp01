3efccb02da6c02a350bf5829e02c60b2c859dd14
SeredaIvan <vt221_bio@student.ztu.edu.ua>
2022-12-04 18:31:33 +0200

    complete

---
+++ SanaCShapr01/LinearExpressions1/Program.cs
@@ -0,0 +1,24 @@
+﻿
+
+Console.WriteLine("Enter a");
+double a = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Enter b");
+double b = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Enter c");
+double c = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Enter d");
+double d = Convert.ToDouble(Console.ReadLine());
+
+double x, y, z, r;
+
+x = ((a + 2 * c - c + d) / c * d) + ((a + b) / (c - d)) - ((a * a) / (b * b));
+y = ((5 * (a + b) * (c - d)) / (1.0 / 2 * c))+(d*d*((a*a-b*b)/(b-a)));
+z = ((x * x - 2 * x * x) * (x * x - 2 * x * x) * (x * x - 2 * x * x) - 4 * (x * x * x * x + 1) * (1 - b) / (5 * a + 3 * b));
+r = (((1.0 / 2 * a) +(3.0/4*b)-7.0/5) / (3*c+1))+((1.0)/(a-c));
+
+
+
+Console.WriteLine(x);
+Console.WriteLine(y);
+Console.WriteLine(z);
+Console.WriteLine(r);
