579d49a67145ae1694005d0c9a581faa35a9fc7b
Volkov <volkov.aliksandr0203@gmail.com>
2022-12-04 05:13:02 +0200

    Created gitignore file and the first exspression

---
+++ LinearExpressions1/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+System.Globalization.CultureInfo customCulture = (System.Globalization.CultureInfo)
+                        System.Threading.Thread.CurrentThread.CurrentCulture.Clone();
+customCulture.NumberFormat.NumberDecimalSeparator = ".";
+System.Threading.Thread.CurrentThread.CurrentCulture = customCulture;
+Console.InputEncoding = System.Text.Encoding.UTF8;
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+Console.Write("Введіть значення змінної A -> ");
+double varA = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введіть значення змінної B -> ");
+double varB = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введіть значення змінної C -> ");
+double varC = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введіть значення змінної D -> ");
+double varD = Convert.ToDouble(Console.ReadLine());
+
+
+double resultX = (varA + 2 * varB - varC + varD) / (varC * varD) + (varA + varB) / (varC - varD) - Math.Pow(varA, 2) / Math.Pow(varB, 2);
+Console.WriteLine(resultX);
